057dc5319e6d307364622946b4d6437904a8b725
Amaan9621 <167350883+Amaan9621@users.noreply.github.com>
2024-05-12 10:37:35 +0200

    Update login.js

--- NutriTracker/public/controllers/login.js
+++ NutriTracker/public/controllers/login.js
@@ -1,11 +1,12 @@
-document.getElementById('redirect-to-register-button').addEventListener('click', function () {
-    window.location.href = 'register.html'
+document.getElementById('redirect-to-register-button').addEventListener('click', function () { // eventlistener aktiveres, når der klikkes på knappen
+    window.location.href = 'register.html' // omdirigerer til registreringssiden
   })
   
+  // Eventlistener til login-knappen (når der klikkes på knappen
   document.getElementById('login-button').addEventListener('click', function () {
-    event.preventDefault()
-    console.log('gdfgdfs')
-  
+    event.preventDefault() 
+    
+    // Hent email og password fra inputfelterne
     const email = document.getElementById('email').value
     const password = document.getElementById('password').value
   
@@ -13,21 +14,21 @@ document.getElementById('redirect-to-register-button').addEventListener('click',
       email: email,
       password: password
     }
-  
+    // Send POST-anmodning til backend
     fetch('http://localhost:3000/api/account/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(data)
-    }).then(function (response) {
+      body: JSON.stringify(data) // konverterer data til JSON-streng
+    }).then(function (response) { //.then() bruges til at håndtere promise
       if (response.status !== 200) {
         return alert('Invalid credentials')
       }
-      return response.json()
-    }).then(function (data) {
+      return response.json() 
+    }).then(function (data) { 
       console.log(data)
-      localStorage.setItem('user', JSON.stringify(data.user))
-      window.location.href = 'mealtracker.html'
+      localStorage.setItem('user', JSON.stringify(data.user)) // gemmer brugeroplysninger i localStorage
+      window.location.href = 'mealtracker.html' // omdirigerer til mealtracker-siden
     })
   })
